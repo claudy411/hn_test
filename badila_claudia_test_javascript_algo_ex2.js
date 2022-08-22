60a1082c2d80e55f11b5497a5af3c3afaed1600d
claudy411 <badila.alina@gmail.com>
2022-08-22 17:00:40 +0200

    second try

--- badila_claudia_test_javascript_algo_ex2.js
+++ badila_claudia_test_javascript_algo_ex2.js
@@ -1,23 +1,35 @@
 
 
 function F21(a) {
-    if (a.length === 9) {
+    // parameter a has to be a row of a table
+    let arr = [];
+    let result = false;
+    let k = 0;
+    do {
+        if (a === document.getElementById(('row' + k))) {
 
-        let arr = [];
-        for (let i = 0; i < a.length; i++) {
-            arr.push(Number(document.getElementsByTagName('td')[i].innerHTML));//get the value of each td of a row
-        }
-        console.log(arr)
-        for (let i = 0; i < arr.length; i++) {
-            if (typeof arr[i] === 'number') {//are there only numbers
+            for (let cell of a.cells) {
 
-                const unique = [...new Set(arr)];//are they all unique
-                if (unique.length === 9) {//if there is a duplicate the length of the unique array is less than 9
-                    return true;
+                arr.push(Number(cell.innerHTML));//get the value of each td of a row and put it inside a array as type number
+            }
+
+            for (let i = 0; i < arr.length; i++) {
+
+                if (typeof arr[i] === 'number' && (arr[i] >= 1 && arr[i] <= 9)) {//are there only numbers between 1 and 9
+
+                    const unique = [...new Set(arr)];//are they all unique
+                    if (unique.length === 9) {//if there is a duplicate the length of the unique array is less than 9
+                     
+                        result = true
+                        return result;
+                    }
                 }
             }
-        }
 
-    }
-    return false;
+        }
+       
+        k++;
+
+    } while (k < 9);
+
 }
--- badila_claudia_test_javascript_algo_ex3.js
+++ badila_claudia_test_javascript_algo_ex3.js
@@ -1,16 +1,23 @@
 document.addEventListener("readystatechange", chargeEvents3, false);
 function chargeEvents3() {
     if (document.readyState === "interactive") {
-        F31();
+        // F31();
+        console.log(F31())
+
     }
 }
 
 function F31() {
-    let fila = [];
-    let table = document.getElementById('to_check');
+    let table= document.getElementById('to_check');
+    let cont=0;
 
-    for (let i = 0; i < table.children.length; i++) {
-        fila.push(document.getElementsByTagName('td')[i].innerHTML);
+    for (let row of table.rows) {
+
+        if(F21(row)===false){
+           let text = document.createTextNode('Line '+ row +' incorrect');
+                row.insertCell(cont).appendChild(text);
+        }
+        cont++;
     }
-
+   
 }
